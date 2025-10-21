13c45b5ba15c8bf593f3f5eda8aff64d3deef5b1
Zaqueu Soares <bartworld14@gmail.com>
2025-10-21 17:06:46 -0300

    update class user

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -0,0 +1,30 @@
+package com.picpaysimplificado.domain.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String document;
+    @Column(unique = true)
+    private String email;
+}
